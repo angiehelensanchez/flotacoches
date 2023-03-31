985ea25ace49523ef7b54226e9dd47838d227aa0
Angie Helen Rodas Sánchez <angiehelensanchez@gmail.com>
2023-03-31 05:51:25 +0000

    31/03/2023

---
+++ AA2_AlquilerVehiculos/src/VehiculosRent.java
@@ -0,0 +1,12 @@
+public class VehiculosRent {
+    public String nif;
+    public String nombre;
+    public Cliente cl;
+    public Flota fl;
+    //constructor
+
+    public VehiculosRent(String nif, String nombre, Cliente cl, Flota fl){
+
+    }
+
+}
